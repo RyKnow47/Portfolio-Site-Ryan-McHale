93e722325d6a81c0903dfd3d8582d1dd6e16807e
RyKnow47 <ryan@ryanui.com>
2021-05-29 23:57:06 -0500

    Create scripts.js
    Created this folder and wrote an alert and console.log text.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/*<button onclick='javascript:alert("You really clicked me!")'>
+  Click me!
+</button>
+
+<script>
+  alert("Hi, thanks for visiting my portfolio!");
+</script>*/
+
+console.log("Hi, Thanks for visiting My Portfolio Site!")
